219fa4f109ddb8178ad3a07b41765d22bc9077b8
pkt0831 <pkt0831@naver.com>
2020-08-06 15:14:14 +0900

    08.06

--- Ex1.js
+++ Ex1.js
@@ -10,16 +10,14 @@
 //   console.log(i)
 // }
 // 3. for문을 사용하여 0부터 10미만의 정수 중에서 짝수만을 작은 수부터 문자열로 출력하시오.
-let str = '';
-for (let i = 0; i < 10; i++) {
-  i = +str;
-}
-console.log(i);
+// let str = '';
+// for (let i = 0; i < 10; i++) {
+//   if (i % 2 === 0) str += i;
+// }
 // console.log(str);
 // 4. for문을 사용하여 0부터 10미만의 정수 중에서 홀수만을 큰수부터 출력하시오.
-// for (let i = 9; i >= 0; i--) {
-//   if (i % 2)
-//     console.log(i);
+// for (let i = 9; i > 0; i--) {
+//   if (i % 2) console.log(i);
 // }
 // 5. while문을 사용하여 0 부터 10 미만의 정수 중에서 짝수만을 작은 수부터 출력하시오.
 // let count = 0;
@@ -28,24 +26,19 @@ console.log(i);
 //   count += 1;
 // }
 // 6. while문을 사용하여 0 부터 10 미만의 정수 중에서 홀수만을 큰수부터 출력하시오.
-// let count = 10;
-// while (count > 0) {
-//   if (count % 2)
-//     console.log(count);
-//   count--;
-// }
+let i = 0;
+while (i < 10) {
+  console.log(i);
+  i++;
+}
 // 7. for 문을 사용하여 0부터 10미만의 정수의 합을 출력하시오.
-// let sum = 0;
+// sum = 0;
 // for (let i = 0; i < 10; i++) {
-//   sum += i;
-// }
-// console.log(sum);
+//   if (sum += i)
+// };
+
 // // 8. 1부터 20 미만의 정수 중에서 2 또는 3의 배수가 아닌 수의 총합을 구하시오.
-// let sum = 0;
-// for (let i = 1; i < 20; i++) {
-//   sum += i % 2 ? (i % 3 ? i : 0) : 0;
-// }
-// console.log(sum);
+
 // // 9. 1부터 20 미만의 정수 중에서 2 또는 3의 배수인 수의 총합을 구하시오.
 // let sum = 0;
 // for (let i = 1; i < 20; i++) {
